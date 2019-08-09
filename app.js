9c92080de11b19a0baea3137200dfae74710832d
saarhaber <saarhaber@gmail.com>
2019-08-09 11:00:45 -0400

    listen for request

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 //this is where our middlewhere and express app will live
+const express = require('express');
+const app = express();
+app.get('/', (req,res,next)=> {
+    res.send("hello world");
+})
+app.listen(3000, ()=> {
+console.log("listening on port 3000");
+});
